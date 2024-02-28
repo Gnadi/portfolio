3caa6cecc25d20b208139ebe5062612c6841fabb
Gnadi <johannes.gnadlinger1997@gmail.com>
2024-02-28 22:17:24 +0100

    added i18n dropdown | added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "de"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  integrations: [tailwind()]
 });
