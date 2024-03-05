f5e3eadf955f4ca24cb3b7095749d99987b6efaa
Gnadi <johannes.gnadlinger1997@gmail.com>
2024-03-05 21:33:24 +0100

    added vercel config SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
-
+// Import /serverless for a Serverless SSR site
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de"],
