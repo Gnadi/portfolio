97571035ae14ca1f0c2c51265a1e2c6cc2af336d
Gnadi <johannes.gnadlinger1997@gmail.com>
2024-03-11 22:43:40 +0100

    removed vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from 'astro/config';
-// Import /static for a static site
-import vercelStatic from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercelStatic(),
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de"],
