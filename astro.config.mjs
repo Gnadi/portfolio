1631f7596dd073fa1602dfe61bb27e7e2dc76b48
Gnadi <johannes.gnadlinger1997@gmail.com>
2024-03-11 21:00:24 +0100

    fixed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,12 @@
 import { defineConfig } from 'astro/config';
-// Import /serverless for a Serverless SSR site
-import vercel from '@astrojs/vercel/serverless';
+// Import /static for a static site
+import vercelStatic from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
+  adapter: vercelStatic(),
   i18n: {
     defaultLocale: "en",
     locales: ["en", "de"],
