ae1cd1f15cc14677f7fb53820e182dacc79711ae
Gnadi <johannes.gnadlinger1997@gmail.com>
2024-05-17 13:38:52 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
@@ -10,5 +12,14 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-  integrations: [tailwind()]
+  site: 'https://www.johannes.gnadlinger.me/',
+  integrations: [tailwind(), sitemap({
+    i18n: {
+      defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+      locales: {
+        en: 'en', // The `defaultLocale` value must present in `locales` keys
+        de: 'de'
+      },
+    },
+  })]
 });
